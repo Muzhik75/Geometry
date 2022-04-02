9e1a81044b5e71e4028e508a4ac7c51e7bb371d0
Muzhik75 <muzhik75@gmail.com>
2022-04-02 19:43:09 +0300

    Домашка

---
+++ Geometry/Figure.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Geometry
+{
+    public abstract class Figure
+    {
+        public string name;
+        public float perimeter;
+        public float square;
+        
+
+        public Figure(string s)
+        {
+            this.name = name;
+            
+        }
+
+        public abstract float Square();
+        public abstract float Perimeter();
+    }
+
+    public class Triangle : Figure
+    {
+        public int numSide;
+        public int firstSide;
+        public int secondSide;
+        public int threedSide;
+
+        public Triangle(string name, int numSide) : base(name)
+        {
+            this.numSide = numSide;
+        }
+
+        public override float Square()
+        {
+            float semiPerimeter = (firstSide + secondSide + threedSide) / 2;
+            float square = (float) Math.Sqrt(semiPerimeter * (semiPerimeter - firstSide) *
+                                                     (semiPerimeter - secondSide) * (semiPerimeter - threedSide));
+            return square;
+        }
+
+
+        public override float Perimeter()
+        {
+            float perimeter = (firstSide + secondSide + threedSide);
+            return perimeter;
+        }
+        
+
+        
+    }
+    
+}
